7b8529d6c3a96e5abaac263e1275c9d4f709570c
vandisi <vinolia.b.19@gmail.com>
2025-04-07 14:02:12 +0300

    feat: Add a folder with a function printing hello world!

---
+++ hello_world/hello_world.rs
@@ -0,0 +1,7 @@
+// This it a rust function for printing a simple string literal of hello world!
+
+fn main() {
+    println!("Hello World!");
+}
+
+// This project has been compiled by rusc(rustc hello_world) , so to run it, use the command "./hello_world"
