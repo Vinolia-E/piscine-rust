916c3406b8fa7913901b291329408229757e4e56
vandisi <vinolia.b.19@gmail.com>
2025-04-08 15:27:16 +0300

    feat: Add a function that allows a guess input and prints the input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+ println!("Guess a number!");
+
+ println!("Kindly input a number:");
+
+ let mut input = String::new();
+
+ io::stdin()
+ .read_line(&mut input)
+ .expect("Failed to read line");
+
+ println!("Your guess is: {}", input);
 }
